75cc1d2fdf3a977e4e29e3e65ae057b57c0f3150
Kornelia Szabo <kszabo@thoughtworks.com>
2019-05-15 16:39:40 +0200

    Updating content with minor details until further dev work

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,7 +8,8 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to kornelia.dev</p>
+    <p>Currently under construction, please check abck later</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
