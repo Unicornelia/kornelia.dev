698569c8b78e536ec1e71250e62b86425cbe474c
Kornelia <ez.kornelia@gmail.com>
2024-02-11 07:37:31 +0100

    fix: smaller fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Welcome to kornelia.dev</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
     <p>Currently under construction, please check back later</p>
-    <Link to="/page-2/">Go to page 2 for links</Link>
   </Layout>
 )
 
