69a067bec8b10aa1c0eac528f81d031d719b7c30
Kornelia <ez.kornelia@gmail.com>
2024-02-11 07:08:17 +0100

    fix: update version, package and config

---
+++ gatsby-config.ts
@@ -0,0 +1,57 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `kornelia.dev`,
+    siteUrl: `https://www.kornelia.dev`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-sitemap", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: "gatsby-plugin-manifest",
+    options: {
+      "icon": "src/images/icon.png",
+    },
+  }, "gatsby-plugin-mdx", {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      "name": "pages",
+      "path": "./src/pages/",
+    },
+    __key: "pages",
+  }, {
+    resolve: "gatsby-plugin-google-gtag",
+    options: {
+      // You can add multiple tracking ids and a page view event will be fired for all of them.
+      trackingIds: [
+        "GA-TRACKING_ID", // Google Analytics / GA
+        "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+        "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+      ],
+      // This object gets passed directly to the gtag config command
+      // This config will be shared across all trackingIds
+      gtagConfig: {
+        optimize_id: "OPT_CONTAINER_ID",
+        anonymize_ip: true,
+        cookie_expires: 0,
+      },
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending page view hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Defaults to https://www.googletagmanager.com
+        origin: "YOUR_SELF_HOSTED_ORIGIN",
+        // Delays processing page view events on route update (in milliseconds)
+        delayOnRouteUpdate: 0,
+      },
+    },
+  }],
+}
+
+export default config
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,8 +8,6 @@ const SecondPage = () => (
   <Layout>
     <SEO title="kornelia.dev" />
     <h1>Link to my <a href='https://github.com/Unicornelia'>Github</a></h1>
-    <h1>Link to my <a href='https://unicornelia.com'>Blog</a></h1>
-
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
