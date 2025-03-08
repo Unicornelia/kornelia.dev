26e38d78b582022ca342dd3f11a79ebda0f9446b
Unicornelia <ez.kornelia@gmail.com>
2025-03-08 17:22:18 +0100

    update gatsby config and packages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,49 +10,65 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-sitemap", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: "gatsby-plugin-manifest",
-    options: {
-      "icon": "src/images/icon.png",
-    },
-  }, "gatsby-plugin-mdx", {
-    resolve: "gatsby-source-filesystem",
-    options: {
-      "name": "pages",
-      "path": "./src/pages/",
-    },
-    __key: "pages",
-  }, {
-    resolve: "gatsby-plugin-google-gtag",
-    options: {
-      // You can add multiple tracking ids and a page view event will be fired for all of them.
-      trackingIds: [
-        "GA-TRACKING_ID", // Google Analytics / GA
-        "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-        "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-      ],
-      // This object gets passed directly to the gtag config command
-      // This config will be shared across all trackingIds
-      gtagConfig: {
-        optimize_id: "OPT_CONTAINER_ID",
-        anonymize_ip: true,
-        cookie_expires: 0,
-      },
-      // This object is used for configuration specific to this plugin
-      pluginConfig: {
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending page view hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Defaults to https://www.googletagmanager.com
-        origin: "YOUR_SELF_HOSTED_ORIGIN",
-        // Delays processing page view events on route update (in milliseconds)
-        delayOnRouteUpdate: 0,
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Kornelia Dev",
+        short_name: "Kornelia",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        display: "minimal-ui",
+        icon: "static/favicon.png",
       },
     },
-  }],
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        // You can add multiple tracking ids and a page view event will be fired for all of them.
+        trackingIds: [
+          "GA-TRACKING_ID", // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending page view hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing page view events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
+  ],
 }
 
 export default config
